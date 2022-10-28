d61852983d8193cb87c4bfff095af6bedb9c9f0e
Salih T <mhdsalih9656@gmail.com>
2022-10-28 14:25:55 +0530

    product model cahnged

--- router/product.js
+++ router/product.js
@@ -46,7 +46,7 @@ productRouter.post("/api/addProduct/:catId/:subCatId",async(req,res)=>{
 productRouter.get("/api/getAll",async(req,res)=>{
 
     try{
-        const allProd = await req.find()
+        const allProd = await productModel.find()
         console.log(allProd);
         res.status(200).json({
             allProd,
