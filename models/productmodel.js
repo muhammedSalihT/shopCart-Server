6d9f30407fd32e9cb89234d432cdf9d6622b7404
Salih T <mhdsalih9656@gmail.com>
2022-10-30 08:33:53 +0530

    error fixed

--- models/productmodel.js
+++ models/productmodel.js
@@ -41,12 +41,11 @@ const productSchema = new mongoose.Schema({
         required:true
     },
     quantity:{type:Number,default:1},
-    images:[{
+    images:{
         img1:{type:String},
         img2:{type:String},
         img3:{type:String},
     }
-    ]
 
 })
 
