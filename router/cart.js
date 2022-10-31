5909b928c2ddb1cf581a58cabd40c9e57da77e78
Salih T <mhdsalih9656@gmail.com>
2022-10-31 22:11:45 +0530

    error fixed

--- router/cart.js
+++ router/cart.js
@@ -10,8 +10,7 @@ const exsistingCart  = await cartModel.findOne({productId});
 if(exsistingCart){
     return res.status(400).json({msg:"This Product is already exsist"});
 }else{
-    cart.save();
-    console.log(cart);
+   const savedCart =await cartModel({productId}).save();
     res.status(200).json({product,msg:"Added Succefully"})
 }
     }catch(e){
