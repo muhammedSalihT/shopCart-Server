82a7d753f8fb609ad317e8c6f901fab39a3d8bb1
Salih T <mhdsalih9656@gmail.com>
2022-11-01 11:28:15 +0530

    api for cart completed

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ const mongoose  = require("mongoose");
 
 const cartShcema = new mongoose.Schema({
    
-   cartitem_Id:{type:mongoose.Schema.Types.ObjectId,required:true},
+   cartitem_Id:{type:mongoose.Schema.Types.ObjectId,ref:"product",required:true},
     user_Id :{
         type:mongoose.Schema.Types.ObjectId,
         ref: 'User',
